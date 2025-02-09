d3eb1951e6d18e31723f66e902cacad5fdbf32db
Ivan Moiseev <iv.mois@yandex.ru>
2025-02-09 15:21:49 +0300

    баннер на день святого валентина

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,55 +1,74 @@
-import styles from './hero.module.sass';
-import Gallery from './gallery';
-import Media from './media';
-import { useContext, useEffect, useRef} from 'react';
-import Background from './background';
-import { widthDeviceContext } from '@/context/widthDeviceContext';
-
+import styles from "./hero.module.sass";
+import Gallery from "./gallery";
+import Media from "./media";
+import { useContext, useEffect, useRef } from "react";
+import Background from "./background";
+import { widthDeviceContext } from "@/context/widthDeviceContext";
 
 const Hero = () => {
-	const sectionRef = useRef<HTMLElement>(null);
-	const widthDevice = useContext(widthDeviceContext);
+  const sectionRef = useRef<HTMLElement>(null);
+  const widthDevice = useContext(widthDeviceContext);
 
-	useEffect(() => {
-		const getVh = () => {
-			const vh = window.innerHeight * 0.01;
-			document.documentElement.style.setProperty('--vh', `${vh}px`);
-		};
+  useEffect(() => {
+    const getVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
 
-		getVh();
-	}, []);
+    getVh();
+  }, []);
 
+  return (
+    <section className={styles.hero} ref={sectionRef} id="hero">
+      <div className={styles.media__container}>
+        {widthDevice && (
+          <div className={styles.media}>
+            {/* {widthDevice > 1023 ? <Gallery /> : <Media />} */}
+            <div className={styles.valentin}>
+              {widthDevice > 1023 ? (
+                <img
+                  className={styles.valentin__img}
+                  alt="valentin"
+                  src="/hero-img/st-valentine.jpg"
+                />
+              ) : (
+                <img
+                  className={styles.valentin__img}
+                  alt="valentin"
+                  src="/hero-img/valentine-mob.jpg"
+                />
+              )}
+            </div>
+          </div>
+        )}
+      </div>
 
-	return (
-		<section className={styles.hero} ref={sectionRef} id='hero'>
-			<div className={styles.media__container}>
-				{widthDevice && <div className={styles.media}>{(widthDevice > 1023 ? <Gallery /> : <Media />)}</div> }
-			</div>
-
-			<div className={styles.descr}>
-				<div className={styles.scroll}>
-					<span>SCROLL</span>
-					<div className={styles.titleLine}>
-						<h1 className={styles.title}>Студия автопортрета SELFIE</h1>
-						<span> &nbsp;{'//'} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;EST. 2023 </span>
-					</div>
-				</div>
-				<p className={styles.slogan}>
-					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;новый
-					<br />
-					формат&nbsp;&nbsp;cъемки,
-					<br />
-					где ты сам себе
+      <div className={styles.descr}>
+        <div className={styles.scroll}>
+          <span>SCROLL</span>
+          <div className={styles.titleLine}>
+            <h1 className={styles.title}>Студия автопортрета SELFIE</h1>
+            <span>
+              {" "}
+              &nbsp;{"//"} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;EST. 2023{" "}
+            </span>
+          </div>
+        </div>
+        <p className={styles.slogan}>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;новый
+          <br />
+          формат&nbsp;&nbsp;cъемки,
+          <br />
+          где ты сам себе
           <br />
           фотограф&nbsp;и модель
-					<br />
-
-				</p>
-				<button className={styles.button}> ЗАБРОНИРОВАТЬ </button>
-			</div>
-			<Background  parentRef={sectionRef}/>
-		</section>
-	);
+          <br />
+        </p>
+        <button className={styles.button}> ЗАБРОНИРОВАТЬ </button>
+      </div>
+      <Background parentRef={sectionRef} />
+    </section>
+  );
 };
 
 export default Hero;
